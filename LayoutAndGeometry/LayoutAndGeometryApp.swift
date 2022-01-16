6f54fe9232f0f5ecbf0e2f513f1c34f613f62c2f
iOSDevHeath <iOSDevHeath@icloud.com>
2022-01-16 08:01:10 -0500

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Heath Fashina on 2022-01-16.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
